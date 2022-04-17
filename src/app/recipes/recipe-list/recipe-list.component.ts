e18d496f6842ee7e32db9648c1aa38bf5cd14862
Interius <intel66666666@gmail.com>
2022-04-17 15:05:18 +0300

    initial project

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,31 @@
+import { Recipe } from "./../recipe.model";
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+
+@Component({
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"],
+})
+export class RecipeListComponent implements OnInit {
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe(
+      "A Test Recipe",
+      "This is simply a test",
+      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"
+    ),
+    new Recipe(
+      "A Test Recipe",
+      "This is simply a test",
+      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
+  }
+}
